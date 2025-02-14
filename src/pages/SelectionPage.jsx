f3fa851871f164fe375887afebdd8a312e43c7b8
Bukola A <bumolaya@gmail.com>
2025-02-13 16:31:27 -0800

    fix: height of selection page

--- src/pages/SelectionPage.jsx
+++ src/pages/SelectionPage.jsx
@@ -3,7 +3,7 @@ import TicketSelection from '../components/TicketSelection';
 
 const SelectionPage = () => {
   return (
-    <div className="gradient w-full h-full md:h-[1082px] py-3  flex-col justify-start items-center gap-20 inline-flex md:overflow-hidden">
+    <div className="gradient w-full h-full py-3  flex-col justify-start items-center gap-20 inline-flex md:overflow-hidden">
       <Nav />
       <div className="py-[56]">
         <TicketSelection />
